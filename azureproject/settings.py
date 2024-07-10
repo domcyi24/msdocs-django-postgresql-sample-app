aead967e981baaa1897945f89b7fc6cb5ff6d05d
Open Data Portal (Department of Meteorology, Cyprus) <165042676+domcyi24@users.noreply.github.com>
2024-07-10 13:17:12 +0300

    Update settings.py -changed debug and allowed hosts

--- azureproject/settings.py
+++ azureproject/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 if 'CODESPACE_NAME' in os.environ:
     CSRF_TRUSTED_ORIGINS = [f'https://{os.getenv("CODESPACE_NAME")}-8000.{os.getenv("GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN")}']
